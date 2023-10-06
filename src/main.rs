2f9c4ab49b8e50eeb456d688afebdb38af4acebc
Peter Becker <peterbecker89@proton.me>
2023-10-06 11:39:27 -0600

    change main to loop over each day

--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,14 @@ fn main() {
     println!("Advent of Code 2022");
     println!("===================");
 
-    let day = 3;
-    let input = fs::read_to_string(format!("inputs/day{day}.txt")).unwrap();
-
-    if let Some(solver) = init_solver(day) {
-        println!("Day {day} Star 1: {}", solver.star_one(&input));
-        println!("Day {day} Star 2: {}", solver.star_two(&input));
+    for day in 1.. {
+        if let Some(solver) = init_solver(day) {
+            let input = fs::read_to_string(format!("inputs/day{day:0>2}.txt")).unwrap();
+            println!("");
+            println!("Day {day:0>2} Star 1: {}", solver.star_one(&input));
+            println!("Day {day:0>2} Star 2: {}", solver.star_two(&input));
+        } else {
+            break;
+        }
     }
 }
